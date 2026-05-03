a5ed90d75da9c07710412a621f1526d1d992ad80
mario hatalcik <majoporche@gmail.com>
2026-05-03 10:16:02 +0200

    feat: add Footer component

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="py-20 text-center border-t border-white/5">
+      <p className="text-[9px] uppercase tracking-[1em] text-neutral-800">
+        Mário Hatalčík &copy; 2024
+      </p>
+    </footer>
+  );
+}
