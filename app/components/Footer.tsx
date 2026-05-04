f1eeac66c59460063903b7a3b78e8a1916b46642
mario hatalcik <majoporche@gmail.com>
2026-05-04 17:46:37 +0200

    fix text email

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,7 +14,7 @@ export function Footer() {
           <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-8 py-10">
             <button
               onClick={handleEmailClick}
-              className="text-3xl md:text-4xl font-bold theme-text-primary hover:theme-text-accent transition-colors text-left w-fit break-all"
+              className="text-xl md:text-4xl text-nowrap font-bold theme-text-primary hover:theme-text-accent transition-colors text-left w-fit break-all"
               style={{ fontFamily: "'Playfair Display', serif" }}
             >
               mario.hatalcik [at] gmail.com
